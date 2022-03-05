a1ac34fad4ef9b3ac1febf5c37fc636a1ad75a51
gimnathperera <gimnath.perera@secureagility.com>
2022-03-05 18:24:49 +0530

    [FIX] custom fonts async loading added

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
 import { DrawerContent } from './screens/DrawerContent';
 import MainTabScreen from './screens/MainTabScreen';
 import LoginScreen from './screens/LoginScreen';
@@ -9,35 +11,55 @@ import BookmarkScreen from './screens/BookmarkScreen';
 import firebase from 'firebase/app';
 import RootStackScreen from './screens/RootStackScreen';
 
-
 export default function App() {
-  
   const Drawer = createDrawerNavigator();
   const [user, setUser] = useState(null);
   useEffect(() => {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
-      	{/* Set a user if someone is logged in */}
+        {
+          /* Set a user if someone is logged in */
+        }
         setUser(user);
       } else {
-        {/* Set user to null, we do this to handle log outs */}
+        {
+          /* Set user to null, we do this to handle log outs */
+        }
         setUser(null);
       }
     });
   }, []);
 
+  let [fontsLoaded] = useFonts({
+    'Kufam-SemiBoldItalic': require('./assets/fonts/Kufam-SemiBoldItalic.ttf'),
+    'Lato-Regular': require('./assets/fonts/Lato-Regular.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
 
   return (
-  <NavigationContainer>
-      {user &&(   
-      <Drawer.Navigator drawerContent={props => <DrawerContent {...props} />}>
-        <Drawer.Screen name="HomeDrawer" component={MainTabScreen} options={{headerShown: false}} />
-        <Drawer.Screen name="SupportScreen" component={SupportScreen} />
-        <Drawer.Screen name="BookmarkScreen" component={BookmarkScreen} />
-        <Drawer.Screen name="Login" component={LoginScreen} options={{headerShown: false}} />
-      </Drawer.Navigator>)}
-       {!user &&(<RootStackScreen/>)}
-       
+    <NavigationContainer>
+      {user && (
+        <Drawer.Navigator
+          drawerContent={(props) => <DrawerContent {...props} />}
+        >
+          <Drawer.Screen
+            name='HomeDrawer'
+            component={MainTabScreen}
+            options={{ headerShown: false }}
+          />
+          <Drawer.Screen name='SupportScreen' component={SupportScreen} />
+          <Drawer.Screen name='BookmarkScreen' component={BookmarkScreen} />
+          <Drawer.Screen
+            name='Login'
+            component={LoginScreen}
+            options={{ headerShown: false }}
+          />
+        </Drawer.Navigator>
+      )}
+      {!user && <RootStackScreen />}
     </NavigationContainer>
   );
 }
